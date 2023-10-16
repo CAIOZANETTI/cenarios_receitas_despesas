526a280b058482f65c7b90a497392b20691cb974
CAIOZANETTI <caiozanetti@gmail.com>
2023-10-16 19:02:38 -0300

    Update 01_criar.py

--- pages/01_criar.py
+++ pages/01_criar.py
@@ -6,13 +6,14 @@ cols =st.columns([1,1,1])
 fluxos = ['aleatorio','parcela-fixa','financiamento price','recorrente']
 cols[0].radio('tipo fluxo',fluxos,key='curva')
 
+st.write(st.session_state['curva'])
+
 if st.session_state['curva'] =='aleatorio':
 
 	cols[1].number_input('min',key='min')
 	cols[1].number_input('max',key='max')
 	#cols[2].write(st.session_state['min'])
 	#cols[2].write(st.session_state['max'])
-	
 
 
 if st.session_state['curva'] =='parcela-fixa':
