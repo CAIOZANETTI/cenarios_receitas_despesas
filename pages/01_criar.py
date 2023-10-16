6faf81f6962b581d7ed70fbbe5408652026d644a
CAIOZANETTI <caiozanetti@gmail.com>
2023-10-16 19:19:38 -0300

    Update 01_criar.py

--- pages/01_criar.py
+++ pages/01_criar.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
 st.write('teste')
+cols =st.columns([1,1,1])
+cols[0].number_input('prazo')
+cols[1].number_input('loops',value=100)
+cols[2].sliden('quartil',5,100,value=50)
 
 cols =st.columns([1,1,1])
 fluxos = ['aleatorio','parcela-fixa','financiamento price','recorrente']
@@ -9,7 +13,7 @@ cols[0].radio('tipo fluxo',fluxos,key='curva')
 st.write(st.session_state['curva'])
 
 if st.session_state['curva'] =='aleatorio':
-	cols[1].write('aleatorio')
+	#cols[1].write('aleatorio')
 
 	cols[1].number_input('min')
 	cols[2].number_input('max')
