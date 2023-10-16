7abd32639a0a2a984006c520d869110695202768
CAIOZANETTI <caiozanetti@gmail.com>
2023-10-16 19:20:27 -0300

    Update 01_criar.py

--- pages/01_criar.py
+++ pages/01_criar.py
@@ -4,7 +4,7 @@ st.write('teste')
 cols =st.columns([1,1,1])
 cols[0].number_input('prazo')
 cols[1].number_input('loops',value=100)
-cols[2].sliden('quartil',5,100,value=50)
+cols[2].slider('quartil',5,100,value=50)
 
 cols =st.columns([1,1,1])
 fluxos = ['aleatorio','parcela-fixa','financiamento price','recorrente']
