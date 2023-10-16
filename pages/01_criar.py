538326ece8243bcf7ac23282c3c9978be66c0a71
CAIOZANETTI <caiozanetti@gmail.com>
2023-10-16 18:48:48 -0300

    app

---
+++ pages/01_criar.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.write('teste')
+
+cols =st.columns([1,1])
+fluxos = ['aleatorio','parcela fixa','financiamento price','recorrente']
+cols[0].radio('tipo fluxo',fluxos,key='curva')
+
+if st.session_state['curva'] =='aleatorio'
+	cols[1].slider(5,10,key='min_max')
+
+
+cols[2].write(st.session_state['min_maxte'])
