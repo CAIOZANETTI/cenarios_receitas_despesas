e16b2d0b3fac10de0987bb28fce6a482ed3c857e
CAIOZANETTI <caiozanetti@gmail.com>
2023-10-17 08:21:03 -0300

    Update 01_criar.py

--- pages/01_criar.py
+++ pages/01_criar.py
@@ -26,7 +26,7 @@ with st.expander('Curva Composta',expanded=True):
 		cols[3].checkbox('inteiro',key='inteiro')
 		cols[4].number_input('min',key='min')
 		cols[5].number_input('max',key='max')
-		cols[3].text('incluir')
+		cols[6].text('incluir')
 		cols[6].button('sim',type='primary',key='btn_incluir')
 
 	if st.session_state['curva'] =='parcela-fixa':
