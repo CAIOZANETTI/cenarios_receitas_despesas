ac3881b55ce258eeaa195698d5c39f596e2a6295
CAIOZANETTI <caiozanetti@gmail.com>
2023-10-17 07:53:27 -0300

    Update 01_criar.py

--- pages/01_criar.py
+++ pages/01_criar.py
@@ -1,24 +1,27 @@
 import streamlit as st
 
-st.write('teste')
-cols =st.columns([1,1,1])
-cols[0].slider('periodo',12,60,value=12)
-cols[1].slider('loops',10,100,value=50)
-cols[2].slider('quartil',5,100,value=50)
+with st.expander('parametros iniciais',expanded=False):
+	cols =st.columns([1,1,1])
+	cols[0].slider('periodo',12,60,value=12)
+	cols[1].slider('loops',10,100,value=50)
+	cols[2].slider('quartil',5,100,value=50)
 
-cols =st.columns([1,1,1])
-fluxos = ['aleatorio','parcela-fixa','financiamento price','recorrente']
-cols[0].radio('tipo fluxo',fluxos,key='curva')
+with st.expander('Curva Composta',expanded=False):
 
-st.write(st.session_state['curva'])
 
-if st.session_state['curva'] =='aleatorio':
-	#cols[1].write('aleatorio')
-
-	cols[1].number_input('min')
-	cols[2].number_input('max')
+	cols =st.columns([1,1,1,1])
+	cols[0].radio('tipo fluxo',['receita','despesa'],key='tipo')
+	fluxos = ['aleatorio','parcela-fixa','financiamento price','recorrente']
+	cols[1].radio('tipo fluxo',fluxos,key='curva')
+	agg = ['+','-','*','/']
 	
-	
-if st.session_state['curva'] =='parcela-fixa':
-	st.write('parcela-fixa')
+	st.write(st.session_state['curva'])
+
+	if st.session_state['curva'] =='aleatorio':
+		cols[2].number_input('min')
+		cols[3].number_input('max')
+		
+		
+	if st.session_state['curva'] =='parcela-fixa':
+		st.write('parcela-fixa')
 
