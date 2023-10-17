9660ab328112fc6c134e6569b75298454c8472dd
CAIOZANETTI <caiozanetti@gmail.com>
2023-10-17 08:05:12 -0300

    criar curvas

--- pages/01_criar.py
+++ pages/01_criar.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
+
+if 'dic' not in st.session_state:
+	st.session_state['dic']={}
 
 with st.expander('parametros iniciais',expanded=False):
 	cols =st.columns([1,1,1])
@@ -6,22 +10,34 @@ with st.expander('parametros iniciais',expanded=False):
 	cols[1].slider('loops',10,100,value=50)
 	cols[2].slider('quartil',5,100,value=50)
 
-with st.expander('Curva Composta',expanded=False):
+with st.expander('Curva Composta',expanded=True):
 
-
-	cols =st.columns([1,1,1,1])
-	cols[0].radio('tipo fluxo',['receita','despesa'],key='tipo')
-	fluxos = ['aleatorio','parcela-fixa','financiamento price','recorrente']
-	cols[1].radio('tipo fluxo',fluxos,key='curva')
-	agg = ['+','-','*','/']
+	cols =st.columns([1,1,1,1,1])
+	cols[0].radio('tipo',['receita','despesa'],key='tipo')
 	
-	st.write(st.session_state['curva'])
+	fluxos = ['aleatorio','parcela-fixa','financiamento price','recorrente']
+	cols[1].radio('distribuição',fluxos,key='curva')
+	
+	fx_math = ['+','-','*','/']
+	cols[2].radio('Agregar',fx_math,key='curva')
 
 	if st.session_state['curva'] =='aleatorio':
+
 		cols[2].number_input('min')
 		cols[3].number_input('max')
-		
+		st.checkbox('inteiro',key='inteiro')
 		
 	if st.session_state['curva'] =='parcela-fixa':
 		st.write('parcela-fixa')
 
+	st.write(st.session_state['curva'])
+
+with st.expander('incluir e remover curva',expanded=True):
+	cols =st.columns([1,1,1])
+	cols[0].button('incluir')
+	cols[1].button('deletar')
+	cols[2].button('limpar')
+
+with st.expander('Resumos',expanded=True):
+	df = pd.DataFrame()
+	st.dataframe(df)
